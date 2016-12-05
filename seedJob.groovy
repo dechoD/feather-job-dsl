571e5e4a19768d3ca167fa4a5505bc5e04b5ca79
Decho Dechev <dechomecho@gmail.com>
2016-12-05 14:48:17 +0200

    dd

--- seedJob.groovy
+++ seedJob.groovy
@@ -5,3 +5,9 @@ new ClientTestJob(
   description: "Runs client side tests for Feather projects and checks code coverage and jshint",
   featherBranch: "*/CodeBaseIntegration",
   emails: "decho.decchev@progress.com tihomir.petrov@progress.com").build(this)
+
+new ClientTestJob(
+  name: "Master_FeatherClientTest",
+  description: "Runs client side tests for Feather projects and checks code coverage and jshint",
+  featherBranch: "*/master",
+  emails: "decho.decchev@progress.com tihomir.petrov@progress.com").build(this)
