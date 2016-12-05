b6110dbd8b384512081a2fe68d3e066f68716a99
Decho Dechev <dechomecho@gmail.com>
2016-12-05 15:32:39 +0200

    adfasdf

--- seedJob.groovy
+++ seedJob.groovy
@@ -12,9 +12,28 @@ new ClientTestJob(
   featherBranch: "*/master",
   emails: "decho.decchev@progress.com tihomir.petrov@progress.com").build(this)
 
+new ClientTestJob(
+  name: "DBP_Codebase_FeatherClientTest",
+  description: "Runs client side tests for Feather projects and checks code coverage and jshint",
+  featherBranch: "*/DBP",
+  emails: "decho.decchev@progress.com tihomir.petrov@progress.com").build(this)
+
 new ClientWidgetsTestJob(
   name: "Codebase_FeatherWidgetsClientTest",
   description: "Runs client side tests for FeatherWidgets projects and checks code coverage and jshint",
   featherBranch: "*/CodeBaseIntegration",
   emails: "decho.decchev@progress.com tihomir.petrov@progress.com").build(this)
+
+new ClientWidgetsTestJob(
+  name: "Master_FeatherWidgetsClientTest",
+  description: "Runs client side tests for FeatherWidgets projects and checks code coverage and jshint",
+  featherBranch: "*/master",
+  emails: "decho.decchev@progress.com tihomir.petrov@progress.com").build(this)
+
+new ClientWidgetsTestJob(
+  name: "DBP_Codebase_FeatherWidgetsClientTest",
+  description: "Runs client side tests for FeatherWidgets projects and checks code coverage and jshint",
+  featherBranch: "*/DBP",
+  emails: "decho.decchev@progress.com tihomir.petrov@progress.com").build(this)
+
 
