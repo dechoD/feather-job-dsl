22c93876b08c560b3c88f2f2ff3a01fc8a92a713
Decho Dechev <dechomecho@gmail.com>
2016-12-07 11:44:25 +0200

    Changed the name in order to test on live feather instance

--- seedJob.groovy
+++ seedJob.groovy
@@ -1,7 +1,7 @@
 import jobs.builders.*
 
 new ClientTestJob(
-  name: "Codebase_FeatherClientTest",
+  name: "Codebase_FeatherClientTest_DSL",
   description: "Runs client side tests for Feather projects and checks code coverage and jshint",
   featherBranch: "*/CodeBaseIntegration",
   emails: "decho.decchev@progress.com tihomir.petrov@progress.com").build(this)
